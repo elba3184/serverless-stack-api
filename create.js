2f26690b1b5abef1b25de3f35e628714a75f6343
Elba Chimilio <emchimilio@gmail.com>
2020-01-20 17:37:14 -0500

    Tested create note function and fixed syntax erros

--- create.js
+++ create.js
@@ -1,7 +1,7 @@
 import * as uuid from 'uuid';
 import AWS from 'aws-sdk';
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 export function main(event, context, callback) {
   // Request body is passed in as a JSON encoded string in 'event.body'
@@ -9,24 +9,32 @@ export function main(event, context, callback) {
 
   const params = {
     TableName: process.env.tableName,
+    // 'Item' contains the attributes of the item to be created
+    // - 'userId': user identities are federated through the
+    //             Cognito Identity Pool, we will use the identity id
+    //             as the user id of the authenticated user
+    // - 'noteId': a unique uuid
+    // - 'content': parsed from request body
+    // - 'attachment': parsed from request body
+    // - 'createdAt': current Unix timestamp
     Item: {
       userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: uuid.v1(), // unique uuid
-      content: data.content, // parsed from request body
-      attachment: data.attachment, // parsed from request body
-      createdAt: Data.now(), // current Unix timestamp
+      noteId: uuid.v1(),
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: Date.now(),
     },
   };
 
-  dynamoDB.put(params, (err, data) => {
-    // Set response headers to enable CORS
+  dynamoDb.put(params, (error, data) => {
+    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
     const headers = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     };
 
     // Return status code 500 on error
-    if (err) {
+    if (error) {
       const response = {
         statusCode: 500,
         headers: headers,
