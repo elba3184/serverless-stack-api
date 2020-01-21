50511a6f4475884dd3f07e9a881bf724be07b5e4
Elba Chimilio <emchimilio@gmail.com>
2020-01-21 16:57:02 -0500

    Refactored some of the create.js code to include dynamo methods and a handler function for any errors

--- create.js
+++ create.js
@@ -1,18 +1,15 @@
 import * as uuid from 'uuid';
-import AWS from 'aws-sdk';
+import handler from './libs/handler-lib';
+import dynamoDb from './libs/dynamodb-lib';
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export function main(event, context, callback) {
+export const main = handler(async (event, context) => {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
 
   const params = {
     TableName: process.env.tableName,
     // 'Item' contains the attributes of the item to be created
-    // - 'userId': user identities are federated through the
-    //             Cognito Identity Pool, we will use the identity id
-    //             as the user id of the authenticated user
+    // - 'userId': user identities are federated through the Cognito Identity Pool
     // - 'noteId': a unique uuid
     // - 'content': parsed from request body
     // - 'attachment': parsed from request body
@@ -26,30 +23,6 @@ export function main(event, context, callback) {
     },
   };
 
-  dynamoDb.put(params, (error, data) => {
-    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-    const headers = {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    };
-
-    // Return status code 500 on error
-    if (error) {
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({ status: false }),
-      };
-      callback(null, response);
-      return;
-    }
-
-    // Return status code 200 and the newly created item
-    const response = {
-      statusCode: 200,
-      headers: headers,
-      body: JSON.stringify(params.Item),
-    };
-    callback(null, response);
-  });
-}
+  await dynamoDb.put(params);
+  return params.Item;
+});
---
+++ libs/handler-lib.js
@@ -0,0 +1,24 @@
+export default function hander(lambda) {
+  return async function(event, context) {
+    let body, statusCode;
+
+    try {
+      // Run the lamda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      body = { error: e.message };
+      statusCode = 500;
+    }
+
+    // Return HTTP Request
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+  };
+}
